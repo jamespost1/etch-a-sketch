1a8b0806cd4bb5a5eda72621b995afbd64e0e976
James Post <jamesbrendanpost@gmail.com>
2025-10-30 13:51:53 -0400

    Start 16x16 grid of divs logic

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let container = document.querySelector(".container")    
+    for (let i = 0; i < 16; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < 16; j++) {
+            let div = document.createElement("div");
+            div.classList.add("box");
+            div.style.backgroundColor = "blue";
+            row.appendChild(div);
+        }
+        container.appendChild(row);
+    }
