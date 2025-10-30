54cd28372082ffac5d7f26ce2300fa411a4edd44
James Post <jamesbrendanpost@gmail.com>
2025-10-30 14:07:58 -0400

    Add hover pen logic

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
-let container = document.querySelector(".container")    
-    for (let i = 0; i < 16; i++) {
-        let row = document.createElement("div");
-        row.classList.add("row");
-        for (let j = 0; j < 16; j++) {
-            let div = document.createElement("div");
-            div.classList.add("box");
-            row.appendChild(div);
-        }
-        container.appendChild(row);
+let container = document.querySelector(".container")
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < 16; j++) {
+        let div = document.createElement("div");
+        div.classList.add("box");
+
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "black";
+        });
+        row.appendChild(div);
     }
+    container.appendChild(row);
+}
